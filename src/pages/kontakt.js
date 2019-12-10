44e6ca5c05db64508d438aaa968fa081e4695c09
Lutando Ngqakaza <lutando@ngqakaza.com>
2019-12-10 17:28:16 +0100

    ok||

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -40,21 +40,21 @@ export default ({ posts, transition, data }) => (
           </p>
         </Block>
         <Block pull mobilePull>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-
+          <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </Block>
       </div>
     </article>
